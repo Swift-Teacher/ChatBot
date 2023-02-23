1351ff3d3fb03cb0eb97774a6b2122d2d6b78957
Brian Foutty <brian@swiftteacher.org>
2023-02-23 12:02:56 -0500

    Initial Commit

---
+++ ChatBot/ConversationDelegate.swift
@@ -0,0 +1,26 @@
+struct ConversationDelegate {
+    /// Creates a String in response to another String.
+    func responseTo(question: String) -> String {
+        let lowerQuestion = question.lowercased()
+        
+        if lowerQuestion.hasPrefix("hello") {
+            return "Why, hello there!"
+        } else if lowerQuestion == "where are the cookies?" {
+            return "In the cookie jar!"
+        } else if lowerQuestion.hasPrefix("where") {
+            return "To the North!"
+        } else if lowerQuestion.contains("what the") {
+            return "Hey, watch your language!!😡🤬"
+        } else {
+            let defaultNumber = question.count % 3
+            
+            if defaultNumber == 0 {
+                return "That really depends"
+            } else if defaultNumber == 1 {
+                return "I think so, yes"
+            } else {
+                return "Ask me again tomorrow"
+            }
+        }
+    }
+}
